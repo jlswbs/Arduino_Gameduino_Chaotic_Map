30a0e39c0a5c0ccc3cb359725de51abe0394eabc
jlswbs <jlswbs@users.noreply.github.com>
2021-09-05 09:35:17 +0200

    Add files via upload

---
+++ Minerva/Minerva.ino
@@ -0,0 +1,48 @@
+// Minerva chaotic map //
+
+#include "SPI.h"
+#include "GD.h"
+
+  byte i;
+  float x = 0.611f;
+  float y = 0.789f;
+  float z = 0.611f;
+       
+void setpixel(byte x, byte y, byte color){
+  
+  unsigned int addr = RAM_SPRIMG|(x & 0xf)|(y << 4)|((x & 0x30)<<8);
+  byte mask = 0xc0 >> ((x >> 5) & 6);
+  GD.wr(addr, (GD.rd(addr) & ~mask) | (color & mask));
+
+}
+
+void setup(){
+  
+  int i;
+  GD.begin();
+  GD.ascii();
+  
+  for (i = 0; i < 256; i++) {
+    int x = 72 + 16 * ((i >> 4) & 15);
+    int y = 22 + 16 * (i & 15); 
+    int image = i & 63;
+    int pal = 3-(i >> 6);
+    GD.sprite(i, x, y, image, 0x8 | (pal << 1), 0,0);
+  }
+  
+  GD.fill(RAM_SPRIMG, 0, 16384);
+  GD.wr16(PALETTE4A + 6, RGB(0,255,255));
+  GD.putstr(15, 1, "Minerva chaotic map");
+  GD.putstr(16, 36, "Created by JLS 2021");
+    
+}
+
+void loop(){
+  
+  x = 1.0f-fabs(0.5f*fabs(y-z)-x);
+  y = 1.0f-fabs(0.5f*fabs(x-z)-y);
+  z = 1.0f-fabs(0.5f*fabs(x-y)-z);
+  
+  setpixel(248*x, 248*y, 255);
+       
+}
