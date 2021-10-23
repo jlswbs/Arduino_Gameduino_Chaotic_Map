d12a286263d21049636003f4ec302c54722590d4
jlswbs <jlswbs@users.noreply.github.com>
2021-10-23 20:39:44 +0200

    Add files via upload

---
+++ Bogdanov/Bogdanov.ino
@@ -0,0 +1,50 @@
+// Bogdanov chaotic map //
+
+#include "SPI.h"
+#include "GD.h"
+
+  byte i;
+  float x = 0.01f;
+  float y = 0.01f;
+  float a = 0.0025f;
+  float b = 1.44f;
+  float c = 0.1f;
+       
+void setpixel(byte x, byte y, byte color){
+  
+  unsigned int addr = RAM_SPRIMG|(x & 0xf)|(y << 4)|((x & 0x30)<<8);
+  byte mask = 0xc0 >> ((x >> 5) & 6);
+  GD.wr(addr, (GD.rd(addr) & ~mask) | (color & mask));
+
+}
+
+void setup(){
+  
+  int i;
+  GD.begin();
+  GD.ascii();
+  
+  for (i = 0; i < 256; i++) {
+    int x = 72 + 16 * ((i >> 4) & 15);
+    int y = 22 + 16 * (i & 15); 
+    int image = i & 63;
+    int pal = 3-(i >> 6);
+    GD.sprite(i, x, y, image, 0x8 | (pal << 1), 0,0);
+  }
+  
+  GD.fill(RAM_SPRIMG, 0, 16384);
+  GD.wr16(PALETTE4A + 6, RGB(0,255,255));
+  GD.putstr(15, 1, "Bogdanov chaotic map");
+  GD.putstr(16, 36, "Created by JLS 2021");
+    
+}
+
+void loop(){
+  
+  float nx = x;
+  x = nx+y+a*y+b*nx*(nx-1)-c*nx*y;
+  y = y+a*y+b*nx*(nx-1)-c*nx*y;
+  
+  setpixel(128+(385*x), 128+(345*y), 255);
+       
+}
