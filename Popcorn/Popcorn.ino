819a0a5a20b525b4b45a28109f34e7e4272f98b0
jlswbs <jlswbs@users.noreply.github.com>
2021-11-21 15:34:57 +0100

    Add files via upload

---
+++ Popcorn/Popcorn.ino
@@ -0,0 +1,49 @@
+// Popcorn chaotic map //
+
+#include "SPI.h"
+#include "GD.h"
+
+  byte i;
+  float x = 0.6f;
+  float y = 0.2f;
+  float a = 0.006f;
+  float b = 3.0f;
+       
+void setpixel(byte x, byte y, byte color){
+  
+  unsigned int addr = RAM_SPRIMG|(x & 0xf)|(y << 4)|((x & 0x30)<<8);
+  byte mask = 0xc0 >> ((x >> 5) & 6);
+  GD.wr(addr, (GD.rd(addr) & ~mask) | (color & mask));
+
+}
+
+void setup(){
+  
+  int i;
+  GD.begin();
+  GD.ascii();
+  
+  for (i = 0; i < 256; i++) {
+    int x = 72 + 16 * ((i >> 4) & 15);
+    int y = 22 + 16 * (i & 15); 
+    int image = i & 63;
+    int pal = 3-(i >> 6);
+    GD.sprite(i, x, y, image, 0x8 | (pal << 1), 0,0);
+  }
+  
+  GD.fill(RAM_SPRIMG, 0, 16384);
+  GD.wr16(PALETTE4A + 6, RGB(0,255,255));
+  GD.putstr(16, 1, "Popcorn chaotic map");
+  GD.putstr(16, 36, "Created by JLS 2021");
+    
+}
+
+void loop(){
+  
+  float nx = x;
+  x = nx - a * sinf(y + tanf(b*y));
+  y = y - a * sinf(nx + tanf(b*nx));
+       
+  setpixel(2+(40*x), 128+(40*y), 255);
+       
+}
