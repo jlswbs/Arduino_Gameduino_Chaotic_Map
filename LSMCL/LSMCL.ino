6664739ba9db365c04fff2ec0410b0bf1b1ddaa0
jlswbs <jlswbs@users.noreply.github.com>
2021-11-27 16:21:10 +0100

    Add files via upload

---
+++ LSMCL/LSMCL.ino
@@ -0,0 +1,54 @@
+// LSMCL chaotic map //
+
+#include "SPI.h"
+#include "GD.h"
+
+  byte i;
+  float x = 0.1f;
+  float y = 0.2f;
+  float a = 0.75f;
+  float b = 3.0f;
+
+void setpixel(byte x, byte y, byte color){
+  
+  unsigned int addr = RAM_SPRIMG|(x & 0xf)|(y << 4)|((x & 0x30)<<8);
+  byte mask = 0xc0 >> ((x >> 5) & 6);
+  GD.wr(addr, (GD.rd(addr) & ~mask) | (color & mask));
+
+}
+
+void setup(){
+  
+  int i;
+  GD.begin();
+  GD.ascii();
+  
+  for (i = 0; i < 256; i++) {
+    int x = 72 + 16 * ((i >> 4) & 15);
+    int y = 22 + 16 * (i & 15); 
+    int image = i & 63;
+    int pal = 3-(i >> 6);
+    GD.sprite(i, x, y, image, 0x8 | (pal << 1), 0,0);
+  }
+  
+  GD.fill(RAM_SPRIMG, 0, 16384);
+  GD.wr16(PALETTE4A + 6, RGB(0,255,255));
+  GD.putstr(17, 1, "LSMCL chaotic map");
+  GD.putstr(16, 36, "Created by JLS 2021");
+    
+}
+
+void loop(){
+  
+  for (int i=0;i<100;i++) {
+
+    float nx = x;
+        
+    x = (sinf(4.0f*PI*a*y*(1.0f-y))+b)*nx*(1.0f-nx);
+    y = (sinf(4.0f*PI*a*nx*(1.0f-nx))+b)*y*(1.0f-y);
+    
+    setpixel(256*x, 256*y, 255);
+    
+  }
+
+}
