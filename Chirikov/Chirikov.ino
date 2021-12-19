089bf59a7eb60d77187b1d4f56412f9b9ad8248b
jlswbs <jlswbs@users.noreply.github.com>
2021-12-19 15:15:27 +0100

    Add files via upload

---
+++ Chirikov/Chirikov.ino
@@ -0,0 +1,49 @@
+// Chirikov chaotic map //
+
+#include "SPI.h"
+#include "GD.h"
+
+  byte i;
+  float a = 0.971635f;
+  float x = PI;
+  float y = PI;
+
+void setpixel(byte x, byte y, byte color){
+  
+  unsigned int addr = RAM_SPRIMG|(x & 0xf)|(y << 4)|((x & 0x30)<<8);
+  byte mask = 0xc0 >> ((x >> 5) & 6);
+  GD.wr(addr, (GD.rd(addr) & ~mask) | (color & mask));
+
+}
+
+void setup(){
+  
+  int i;
+  GD.begin();
+  GD.ascii();
+  
+  for (i = 0; i < 256; i++) {
+    int x = 72 + 16 * ((i >> 4) & 15);
+    int y = 22 + 16 * (i & 15); 
+    int image = i & 63;
+    int pal = 3-(i >> 6);
+    GD.sprite(i, x, y, image, 0x8 | (pal << 1), 0,0);
+  }
+  
+  GD.fill(RAM_SPRIMG, 0, 16384);
+  GD.wr16(PALETTE4A + 6, RGB(0,255,255));
+  GD.putstr(16, 1, "Chirikov chaotic map");
+  GD.putstr(16, 36, "Created by JLS 2021");
+    
+}
+
+void loop(){
+
+  float nx = x;
+       
+  x = nx + (a/TWO_PI) * sinf(TWO_PI*y);
+  y = fmod(y+x, 1.0f);
+  
+  setpixel(-700+(270*x), 256*y, 255);
+
+}
